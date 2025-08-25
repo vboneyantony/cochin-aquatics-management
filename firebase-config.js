d04a7367277d46ddff753a05d8a7b821b4dbc6e2
vboneyantony <vboneyantony@gmail.com>
2025-08-25 23:26:09 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,11 +3,11 @@
 const firebaseConfig = {
     apiKey: "AIzaSyASiFSwx9XWnwqRu07YVCmvJ8Txg-t_eLY",
     authDomain: "cochin-aquatics-manageme-bf543.firebaseapp.com",
-    databaseURL: "https://your-project-default-rtdb.firebaseio.com/",
+    databaseURL: "https://cochin-aquatics-manageme-bf543-default-rtdb.firebaseio.com/",
     projectId: "cochin-aquatics-manageme-bf543",
     storageBucket: "cochin-aquatics-manageme-bf543.firebasestorage.app",
     messagingSenderId: "625465354366",
-    appId: "1:625465354366:web:4985c0c7e5daf4405f102f"
+    appId: "1:625465354366:web:4985c0c7e5daf4405f102f",
     measurementId: "G-WYTZ6G2XN0"
 };
 
@@ -19,4 +19,5 @@ const auth = firebase.auth();
 const database = firebase.database();
 
 console.log('🔥 Firebase initialized successfully!');
+
 
